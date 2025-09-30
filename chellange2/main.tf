5c4ead92c55d383149d68da2766582d19532a2e2
Markiyan Pyts <markiyan.pyts@gmail.com>
2025-09-30 13:55:46 +0300

    ec2

--- chellange2/main.tf
+++ chellange2/main.tf
@@ -2,75 +2,90 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_instance" "db_ec2" {
-    ami = "ami-0444794b421ec32e4"
-    instance_type = "t2.micro"
-    
-    tags = {
-      Name = "DB Server"
-    }
+data "aws_vpc" "default" {
+  default = true
 }
 
-output "PrivateIP" {
-    value = aws_instance.db_ec2.private_ip
-}
+resource "aws_security_group" "webtraffic" {
+  name        = "allow-web"
+  description = "Allow web + optional SSH"
+  vpc_id      = data.aws_vpc.default.id
 
-resource "aws_instance" "web_server" {
-    ami = "ami-0444794b421ec32e4"
-    instance_type = "t2.micro"
-    security_groups = [aws_security_group.webtraffic.name]
-
-    tags = {
-      Name = "Web Server"
+  dynamic "ingress" {
+    for_each = toset(var.ingressrules_ports)
+    content {
+      from_port         = ingress.value
+      to_port           = ingress.value
+      protocol          = "tcp"
+      cidr_blocks       = ["0.0.0.0/0"]
+      ipv6_cidr_blocks  = ["::/0"]
+      description       = "allow ingress port ${ingress.value} from anywhere"
     }
+  }
 
-    user_data = file("server-script.sh")
+  # optional SSH while testing
+  ingress {
+    from_port         = 22
+    to_port           = 22
+    protocol          = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"]
+    ipv6_cidr_blocks  = ["::/0"]
+    description       = "temporary SSH"
+  }
 
+  # wide-open egress (common default)
+  egress {
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1"
+    cidr_blocks       = ["0.0.0.0/0"]
+    ipv6_cidr_blocks  = ["::/0"]
+    description       = "allow all egress"
+  }
 }
 
 variable "ingressrules_ports" {
-    type = list(number)
-    default = [443, 80]
+  type    = list(number)
+  default = [80, 443]
 }
 
-variable "egressrules_ports" {
-    type = list(number)
-    default = [443, 80]
+resource "aws_instance" "db_ec2" {
+  ami           = "ami-0444794b421ec32e4" # verify this for eu-central-1
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "DB Server"
+  }
 }
 
-resource "aws_eip" "elasticeip" {
-  instance = aws_instance.web_server.id
+output "PrivateIP" {
+  value = aws_instance.db_ec2.private_ip
+}
+
+resource "aws_instance" "web_server" {
+  ami                    = "ami-0444794b421ec32e4" # verify
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.webtraffic.id]
+
+  # Make sure server-script.sh has a shebang and installs Apache
+  user_data = file("server-script.sh")
+
+  tags = {
+    Name = "Web Server"
+  }
+}
+
+# Allocate an Elastic IP in the VPC
+resource "aws_eip" "web" {
+  domain = "vpc"
+}
+
+# Associate it to the web instance
+resource "aws_eip_association" "web" {
+  allocation_id = aws_eip.web.allocation_id
+  instance_id   = aws_instance.web_server.id
 }
 
 output "PublicIP" {
-    value = aws_eip.elasticeip.public_ip
-}
-
-# Use dynamic blocks to generate ingress/egress rules from the variables.
-resource "aws_security_group" "webtraffic" {
-    name = "Allow HTTPS"
-
-    dynamic "ingress" {
-      for_each = var.ingressrules_ports
-      content {
-        from_port       = ingress.value
-        to_port         = ingress.value
-        protocol        = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-        description     = "allow ingress port ${ingress.value} from anywhere"
-      }
-    }
-
-    dynamic "egress" {
-      for_each = var.egressrules_ports
-      content {
-        from_port       = egress.value
-        to_port         = egress.value
-        protocol        = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-        description     = "allow egress port ${egress.value} to anywhere"
-      }
-    }
+  value = aws_eip.web.public_ip
 }
--- chellange2/server-script.sh
+++ chellange2/server-script.sh
@@ -1,5 +1,6 @@
-sudo yum update
-sudo yum install -y httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
-echo "<h1>Hello from Terraform</h1>" | sudo tee /var/www/html/index.html
+#!/bin/bash
+set -euxo pipefail
+yum update -y
+yum install -y httpd
+systemctl enable --now httpd
+echo "<h1>Hello from Terraform</h1>" > /var/www/html/index.html
