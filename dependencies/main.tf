37ad62132c779cbc17e2c79dea1d80b1e03d3de8
Markiyan Pyts <markiyan.pyts@gmail.com>
2025-10-01 22:15:26 +0300

    deps

---
+++ dependencies/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "db" {
+    ami = "ami-0444794b421ec32e4"
+    instance_type = "t2.micro"
+}
+
+resource "aws_instance" "web" {
+    ami = "ami-0444794b421ec32e4"
+    instance_type = "t2.micro"
+
+    depends_on = [ aws_instance.db ]
+}
