16a55e02878282fff91e766df699bbb10e785fa5
Markiyan Pyts <markiyan.pyts@gmail.com>
2025-10-02 16:48:03 +0300

    more work with terragrunt

--- terragrunt/shared/main.tf
+++ terragrunt/shared/main.tf
@@ -1,6 +1,12 @@
 variable "content" {}
+variable "output_dir" {}
 
+# resource "local_file" "file" {
+  
+#   content  = var.content
+#   filename = "${path.module}/hi.txt"
+# }
 resource "local_file" "file" {
   content  = var.content
-  filename = "${path.module}/hi.txt"
+  filename = "${var.output_dir}/hi.txt"
 }
